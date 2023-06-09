4d9fc65e5818f30a64c85eeb1a6e8901e67e242c
r-mutax <magicspell.max.mix.mutax@gmail.com>
2023-06-10 07:31:14 +0900

    return文の追加

--- test.sh
+++ test.sh
@@ -17,37 +17,38 @@ assert() {
 }
 
 # test num
-assert 0 "0;"
-assert 42 "42;"
+assert 0 "return 0;"
+assert 42 "return 42;"
 
 # test + - * /
-assert 8 "3+5;"
-assert 3 "8-5;"
-assert 13 "1 + 3 * 4;"
-assert 5 "3 + 4 / 2;"
-assert 5 "(9 + 6) / 3;"
+assert 8 "return 3+5;"
+assert 3 "return 8-5;"
+assert 13 "return 1 + 3 * 4;"
+assert 5 "return 3 + 4 / 2;"
+assert 5 "return (9 + 6) / 3;"
 
 # test equality
-assert 1 "2 == 2;"
-assert 0 "2 == 3;"
-assert 1 "3 + 4 != 8;"
-assert 0 "3 + 4 != 7;"
+assert 1 "return 2 == 2;"
+assert 0 "return 2 == 3;"
+assert 1 "return 3 + 4 != 8;"
+assert 0 "return 3 + 4 != 7;"
 
 # test relational
-assert 1 "3 < 4;"
-assert 0 "3 < 2;"
-assert 1 "3 <= 3;"
-assert 1 "3 <= 4;"
-assert 0 "3 <= 2;"
-assert 1 "4 > 3;"
-assert 1 "4 >= 4;"
+assert 1 "return 3 < 4;"
+assert 0 "return 3 < 2;"
+assert 1 "return 3 <= 3;"
+assert 1 "return 3 <= 4;"
+assert 0 "return 3 <= 2;"
+assert 1 "return 4 > 3;"
+assert 1 "return 4 >= 4;"
 
 # test statement
-assert 5 "3; 5;"
+assert 3 "return 3; 5;"
 
 # test lvar
-assert 3 "a=3; a;"
-assert 5 "a=5; b = 3; a;"
+assert 3 "return a=3; a;"
+assert 5 "return a=5; b = 3; a;"
+assert 3 "return abc=3; abc;"
 
 echo OK
 
