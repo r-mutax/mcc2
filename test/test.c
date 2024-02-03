c7e3c1eb8edb194bba321d35c8b0be61f8578f74
Ryo Muta <50309138+r-mutax@users.noreply.github.com>
2024-02-03 23:46:52 +0000

    zscdfa

--- test/test.c
+++ test/test.c
@@ -1,9 +1,16 @@
 // test function define.
 int printf(char* fmt, ...);
-
+int func();
+int funcA(int a, int b) {
+    return a + b;
+}
 
 int main(){
-    printf("mcc2 test...\n\n");
-
+    func();
+    funcA(3, 4);
     return 0;
+}
+
+int func(){
+    return 3;
 }
