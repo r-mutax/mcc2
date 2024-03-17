a437e573505def585adeed61d7523ef3dde6176b
r-mutax <magicspell.max.mix.mutax@gmail.com>
2024-03-17 20:53:26 +0900

    Warningメッセージを出せるようにする2

--- src/error.c
+++ src/error.c
@@ -124,6 +124,45 @@ void error_at_src(char* pos, SrcFile* src, char* fmt, ...){
     exit(1);
 }
 
+void warn_at_src(char* pos, SrcFile* src, char* fmt, ...){
+    char* user_input = src->body;
+    char* filename = src->name;
+
+    // 行頭を見つける
+    char *line = pos;
+    while (user_input < line && line[-1] != '\n')
+        line--;
+
+    // 行末を見つける
+    char *end = pos;
+    while (*end != '\n')
+        end++;
+
+    // 行数を数える
+    int line_num = 1;
+    for (char *p = user_input; p < line; p++){
+        if (*p == '\n'){
+            line_num++;
+        }
+    }
+
+    // エラー箇所を表示する
+    int indent = fprintf(stderr, "%s:%d: ", filename, line_num);
+    fprintf(stderr, "%.*s\n", (int)(end - line), line);
+
+    // 空白を表示する
+    int pos_ = pos - line + indent;
+    fprintf(stderr, "%*s", pos_, " ");
+
+    // エラー箇所を示して、メッセージを表示する
+    fprintf(stderr, "^ ");
+
+    va_list ap;
+    va_start(ap, fmt);
+    vfprintf(stderr, fmt, ap);
+    fprintf(stderr, "\n");
+}
+
 void error(char* fmt, ...){
     va_list ap;
     va_start(ap, fmt);
--- src/error.h
+++ src/error.h
@@ -4,5 +4,6 @@
 void error_tok(Token* tok, char* fmt, ...);
 void warn_tok(Token* tok, char* fmt, ...);
 void error_at_src(char* pos, SrcFile* src, char* fmt, ...);
+void warn_at_src(char* pos, SrcFile* src, char* fmt, ...);
 void error(char* fmt, ...);
 void unreachable();
