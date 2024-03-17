8961be6e42fb77f07a90d8b30b5cb52fdd751d24
r-mutax <magicspell.max.mix.mutax@gmail.com>
2024-03-17 20:51:22 +0900

    Warningメッセージを出せるようにする

--- src/error.c
+++ src/error.c
@@ -3,6 +3,46 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void warn_tok(Token* tok, char* fmt, ...){
+    va_list ap;
+    va_start(ap, fmt);
+
+    char* user_input = tok->file->body;
+    char* filename = tok->file->name;
+    char* loc = tok->pos;
+
+    // 行頭を見つける
+    char *line = loc;
+    while (user_input < line && line[-1] != '\n')
+        line--;
+
+    // 行末を見つける
+    char *end = loc;
+    while (*end != '\n')
+        end++;
+
+    // 行数を数える
+    int line_num = 1;
+    for (char *p = user_input; p < line; p++){
+        if (*p == '\n'){
+            line_num++;
+        }
+    }
+
+    // エラー箇所を表示する
+    int indent = fprintf(stderr, "%s:%d: ", filename, line_num);
+    fprintf(stderr, "%.*s\n", (int)(end - line), line);
+
+    // 空白を表示する
+    int pos = loc - line + indent;
+    fprintf(stderr, "%*s", pos, " ");
+
+    // エラー箇所を示して、メッセージを表示する
+    fprintf(stderr, "^ ");
+    vfprintf(stderr, fmt, ap);
+    fprintf(stderr, "\n");
+}
+
 void error_tok(Token* tok, char* fmt, ...){
     char* user_input = tok->file->body;
     char* filename = tok->file->name;
