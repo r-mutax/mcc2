6203f778305fcaae8d286951400c0c425316de32
Ryo Muta <50309138+r-mutax@users.noreply.github.com>
2025-03-21 15:24:31 +0000

    CFAを追えるようにする

--- dev/dwarf_test.c
+++ dev/dwarf_test.c
@@ -2,6 +2,15 @@
 
 int a = 1;
 char c = 10;
+
+int func(int a, int b){
+    int c = a + b;
+    return c;
+}
 int main(int a){
+    int abc = 10;
+    int k[5];
+    abc *= a;
+    abc = func(1, 2);
     return 0;
 }
